835f60a858a5062321b67d171e9c3014bae717d1
Han Xiaoshen <84143195+hanleo001@users.noreply.github.com>
2023-06-19 14:54:54 +0800

    x

--- animal.py
+++ animal.py
@@ -7,21 +7,15 @@ def default():
     print("Hello")
 
 def dog():
-    print("Wolf")
+    print("Wolf.")
 
 def main():
-<<<<<<< HEAD
     if sys.argv[1] == 'cat':
         cat()
-    else:
-    default()
-=======
-    if sys.argv[1] != "dog":
-        default()
-    else:
+    elif sys.argv[1] == 'dog':
         dog()
->>>>>>> dog
-
-
+    else:
+        default()
+    
 if __name__ == "__main__":
     main()
